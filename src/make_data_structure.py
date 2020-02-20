960853d012fd8419dee8528a179637b5552088f2
allen44 <allen44@users.noreply.github.com>
2020-02-19 14:48:23 -1000

    Fairly stable now

--- src/make_data_structure.py
+++ src/make_data_structure.py
@@ -1,34 +1,40 @@
-def make_dict(data_field_names, rows):
+def make_dict(headers, data_entries):
     data_structure = []
-    data_field_names.insert(0, 'Row Number')
-    print(rows)
-    print(data_field_names)
-    keys = tuple(data_field_names)
+    # headers.insert(0, 'Row Number')
+    print(data_entries)
+    print(headers)
+    keys = tuple(headers)
     print(keys)
-    values = rows[0]
-    print('\nrows[0]\n', rows[0])
-    for i in range(len(rows)):
-        for idx, data in enumerate(rows[i]):
-            values = rows[i]
-            new_dict = dict(zip(keys, values))
-            data_structure.append(new_dict)
-            print('\nprint(new_dict)\n', new_dict)
+    values = data_entries[0]
+    print('\data_entries[0]\n', data_entries[0])
+    for i in range(len(data_entries)):
+        values = data_entries[i]
+        new_dict = dict(zip(keys, values))
+        data_structure.append(new_dict)
+        print('\nprint(new_dict)\n', new_dict)
     return data_structure
 
-
-
 while __name__ == '__main__':
-    import read_write_functions 
+    import read_write_functions
+    import pprint
     input = './input/data.csv'
     output = './output/report.csv'
+    # relavent_fieldnames = {'Border', 'Date', 'Measure', 'Value'}
+    headers1, data_entries1 = read_write_functions.read_csvfile_to_memory(input)
+    print ('headers1', headers1)
+    print('data_entries1')
+    pprint.pprint(data_entries1)
+    # data_entries2 = import_csv_with_dictreader(input, relavent_fieldnames)
+    # print('\ndata_entries2\n')
+    # pprint.pprint(data_entries2)
+    # print('\nprint(data_entries2)\n', data_entries2)
+    # print(type(data_entries2))    
     import parse_data
-    raw_data1 = read_write_functions.read_csvfile_to_memory(input)
-    #print('\nprint(raw_data)\n', raw_data)
-    #print('\nprint(raw_data[0])\n', raw_data[0])
-    data_field_names1 = parse_data.data_field_names(raw_data1)
-    print('\nprint(data_field_names1)\n', data_field_names1)
-    rows1 = parse_data.rows(raw_data1, data_field_names1)
-    print('\nprint(rows1)\n', rows1)
-    make_dict1 = make_dict(data_field_names1, rows1)
+    import make_data_structure
+    make_dict1 = make_data_structure.make_dict(headers1, data_entries1)
     print('\nprint(make_dict1)\n', make_dict1)
+    import sort_dictionary_values
+    sorted_dict1 = sort_dictionary_values.sort_dict_by_values(make_dict1)
+    print('\nprint(sorted_dict1)\n', sorted_dict1)
+    read_write_functions.export_csv_with_dictwriter(output, headers1, sorted_dict1)
     break
--- src/read_write_functions.py
+++ src/read_write_functions.py
@@ -1,72 +1,89 @@
 import csv
 
+
 def read_csvfile_to_memory(input_filename):
     """
     Args: a string representing a filename of csv file.
     Return: a list of strings representing column names from the top row of the csv, and the data fields from the rest of the csvfile.
     """
-    raw_data = []
+    data_entries = []
+    headers = []
     with open(input_filename, 'r',) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        for row in (reader):
-                raw_data.append(row)
-    return(raw_data)
+        for row in reader:
+            data_entries.append(row)
+    headers = data_entries.pop(0) 
+    print(headers)
+    print(data_entries)
+    return(headers, data_entries)
     
-def write_memory_to_csvfile(output_filename, raw_data):
-    """
-    Args: a string representing a filename of output csv file.
-    """
-    raw_data = []
-    with open(output_filename, 'w',) as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for row in writer:
-                writer.writerow(row)
-    return None
+# def write_memory_to_csvfile(output_filename, data_entries):
+#     """
+#     Args: a string representing a filename of output csv file.
+#     """
+#     data_entries = []
+#     with open(output_filename, 'w',) as csvfile:
+#         writer = csv.writer(csvfile, delimiter=',')
+#         for row in writer:
+#                 writer.writerow(row)
+#     return None
 
-def import_csv_with_dictreader(input_filename):
-    """
-    Args: a string representing a filename of input csv file.
-    Return: a list of ordered dicts representing column names from the top row of the csv,as the keys, and the data fields from the rest of the csvfile as the values.
-    """
-    reader = csv.DictReader(open(input_filename))
-    raw_data = []
-    for row in (reader):
-        raw_data.append(row)
-    return raw_data
-
-def export_csv_with_dictwriter(output_filename, data_field_names, sorted_dict):
+# def import_csv_with_dictreader(input_filename, relavent_fieldnames):
+#     """
+#     Args: a string representing a filename of input csv file.
+#     Return: a list of ordered dicts representing column names from the top row of the csv,as the keys, and the data fields from the rest of the csvfile as the values.
+#     """
+#     data_entries = []
+#     reader = csv.DictReader(open(input_filename))
+#     for row in (reader):
+#         keys = (row.items())
+#         print(keys)
+#         print('\nrow\n', row)
+#         print('items', list(keys))
+#     #print('\nfieldnames\n',type(fieldnames))
+#     #print('\nfieldnames\n',fieldnames)
+#     return data_entries
+    
+def export_csv_with_dictwriter(output_filename, headers, sorted_dict):
     """
     Args: a string representing a filename of input csv file.
     Return: a list of ordered dicts representing column names from the top row of the csv,as the keys, and the data fields from the rest of the csvfile as the values.
     """
     try:
         with open(output_filename, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=data_field_names)
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
             writer.writeheader()
             for data in sorted_dict:
                 writer.writerow(data)
+            print("Success. Export to csv complete")
     except IOError:
         print("I/O error")
 
 while __name__ == '__main__':
-    import read_write_functions 
+    import read_write_functions
+    import pprint
     input = './input/data.csv'
     output = './output/report.csv'
-    raw_data1 = read_csvfile_to_memory(input)
-    print('\nprint(raw_data1)\n', raw_data1)
-    raw_data2 = import_csv_with_dictreader(input)
-    print('\nprint(raw_data2)\n', raw_data2)
-    print(type(raw_data2))    
+    # relavent_fieldnames = {'Border', 'Date', 'Measure', 'Value'}
+    headers1, data_entries1 = read_csvfile_to_memory(input)
+    print ('headers1', headers1)
+    print('data_entries1')
+    pprint.pprint(data_entries1)
+    # data_entries2 = import_csv_with_dictreader(input, relavent_fieldnames)
+    # print('\ndata_entries2\n')
+    # pprint.pprint(data_entries2)
+    # print('\nprint(data_entries2)\n', data_entries2)
+    # print(type(data_entries2))    
     import parse_data
-    data_field_names1 = parse_data.data_field_names(raw_data1)
-    print('\nprint(data_field_names1)\n', data_field_names1)
-    rows1 = parse_data.rows(raw_data1, data_field_names1)
-    print('\nprint(rows1)\n', rows1)
+    # headers = parse_data.headers(data_entries1)
+    # print('\nprint(data_field_names1)\n', data_field_names1)
+    # data_entries1 = parse_data.rows(data_entries1, data_field_names1)
+    # print('\nprint(data_entries1)\n', data_entries1)
     import make_data_structure
-    make_dict1 = make_data_structure.make_dict(data_field_names1, rows1)
+    make_dict1 = make_data_structure.make_dict(headers1, data_entries1)
     print('\nprint(make_dict1)\n', make_dict1)
     import sort_dictionary_values
     sorted_dict1 = sort_dictionary_values.sort_dict_by_values(make_dict1)
     print('\nprint(sorted_dict1)\n', sorted_dict1)
-    csv_export1 = export_csv_with_dictwriter(output, data_field_names1, sorted_dict1)
+    export_csv_with_dictwriter(output, headers1, sorted_dict1)
     break
