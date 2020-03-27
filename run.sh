e415789ac8d7ff2fa7f25a17fa16156d9afa907c
allen44 <allen44@users.noreply.github.com>
2020-03-26 18:05:56 -1000

    Deleted the unecessary run.sh copy. Saved changes after uncommenting of run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+python3.7 ./src/main.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+    
